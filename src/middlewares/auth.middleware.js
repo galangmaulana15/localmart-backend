acecbeccecc5c98d626d8b91fc56d3761edd399d
galangmaulana15 <galangponco123@gmail.com>
2026-06-14 14:17:17 +0700

    fix auth middleware bearer token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,36 +1,35 @@
 import { verifyToken } from "../utils/jwt.js";
 
-export const authMiddleware = (
-  req,
-  res,
-  next
-) => {
-
+export const authMiddleware = (req, res, next) => {
   try {
+    let token = null;
 
-    const token = req.cookies.token;
+    const authHeader = req.headers.authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      token = authHeader.split(" ")[1];
+    }
+
+    if (!token && req.cookies?.token) {
+      token = req.cookies.token;
+    }
 
     if (!token) {
       return res.status(401).json({
         success: false,
-        message: "Token tidak ditemukan"
+        message: "Token tidak ditemukan",
       });
     }
 
-    const decoded =
-      verifyToken(token);
+    const decoded = verifyToken(token);
 
     req.user = decoded;
 
     next();
-
   } catch (error) {
-
     return res.status(401).json({
       success: false,
-      message: "Token tidak valid"
+      message: "Token tidak valid",
     });
-
   }
-
 };
